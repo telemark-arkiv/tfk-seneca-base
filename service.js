f3dbb78880959806f1ffaaf7a19393baf25353af
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-04-20 14:00:28 +0200

    Initial commit

---
+++ service.js
@@ -0,0 +1,17 @@
+'use strict'
+
+var Seneca = require('seneca')
+var Mesh = require('seneca-mesh')
+
+var envs = process.env
+var options = {
+  seneca: {
+    tag: envs.TFK_SENECA_BASE_TAG || 'tfk-seneca-base'
+  },
+  mesh: {
+    base: true
+  }
+}
+
+Seneca(options.seneca)
+  .use(Mesh, options.mesh)
